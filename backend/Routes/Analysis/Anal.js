ef5f242fc13f875681237bdaca5de6194b85e10d
Shaheen <46461452+cpshaheen@users.noreply.github.com>
2021-07-02 15:48:15 -0700

    now handling duplicate sets (same fiat, bdate, address)
    modified POST '/Anal/' to handle duplicates by prompting front end with number of dups when dups are found so user can decide if they wish to overwrite or to create a duplicate set

--- backend/Routes/Analysis/Anal.js
+++ backend/Routes/Analysis/Anal.js
@@ -165,58 +165,60 @@ router.post("/Cal", function (req, res) {
 	);
 });
 
-router.post("/", function (req, res) {
-	var vld = req.validator;
-	var body = req.body;
-	var prsId = req.session.prsId;
-	console.log(body);
-	async.waterfall(
-		[
-			function (cb) {
-				console.log("BODY: ", body);
-				if (vld.hasFields(body, ["address", "fiat", "basisDate"]))
-					User.find({ _id: prsId }, function (err, docs) {
-						if (err) cb(err);
-						cb(null, docs);
-					});
-			},
-			function (userInfo, cb) {
-				if (
-					vld.check(
-						!userInfo[0]["addresses"][body["address"]],
-						Tags.dupAddress,
-						null,
-						cb
-					)
-				) {
-					// create a new copy and add the address to the users
-					// address list/obj
-					let oldAddresses = userInfo[0]["addresses"];
-					oldAddresses[body["address"]] = {
-						fiat: body["fiat"],
-						basisDate: body["basisDate"],
-					};
-					let userCopy = { ...userInfo[0] }._doc;
-					userCopy.addresses = oldAddresses;
-					User.findOneAndUpdate(
-						{ _id: userCopy._id },
-						userCopy,
-						function (err, doc) {
-							if (err) cb(err);
-							cb(null, doc);
-						}
-					);
-				}
-			},
-			function (doc, cb) {
-				res.location(router.baseURL + "/" + body["address"]).end();
-				cb();
-			},
-		],
-		function (err) {
-			if (err) console.log(err);
-		}
-	);
+router.post('/', function(req, res) {
+    var vld = req.validator;
+    var body = req.body;
+    var prsId = req.session.prsId;
+    
+    async.waterfall([
+        function(cb){
+            if (vld.hasFields(body, ["address","fiat","basisDate"]))
+                User.find({_id: prsId}, function(err, docs){
+                    if(err) cb(err);
+                    cb(null, docs)
+                })
+        },
+        function(userInfo, cb){
+            // if a dup exists send a body with 'dup check' to indicate to the
+            // front end that they should prompt the user with a dialogue to manage
+            // duplicates
+            userInfo = userInfo[0]
+            if(userInfo.setIds.length !== 0){
+                res.status(200).json({'dup check':userInfo.setIds.length})
+                cb(null,null);
+            }
+            // if no dups exist, create a new realize history object
+            else{
+                rel_obj = new RealizeHistObj({
+                    userid: userInfo._id,
+                    version: 0,
+                    fiat: body["fiat"],
+                    address: body["address"],
+                    basisDate: body["basisDate"]
+                })
+                rel_obj.save(function(err, doc){
+                    if(err) cb(err);
+                    cb(null,doc);
+                })
+            }
+        },
+        function(rel_doc, cb){
+            // associate the new realize history obj with the user
+            if(rel_doc){
+                User.findOneAndUpdate({_id: prsId}, {$addToSet: {"setIds": rel_doc._id}}, 
+                    function(err, doc){
+                        if(err) cb(err);
+                        cb(null, doc);
+                })
+            }
+        },
+        function(doc, cb){
+            res.location(router.baseURL + '/' + body["address"]+'v0').end();
+            cb();
+        }],
+        function(err){
+            if(err) console.log(err);
+        });
 });
 
 Date.prototype.addDays = function (days) {
