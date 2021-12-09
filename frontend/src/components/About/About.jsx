88f89cce3b03966d9cb332ff3177326a55525689
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2021-12-08 22:25:44 -0800

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -11,19 +11,19 @@ const About = () => {
 				</h1>
                 <br />
 				<br />
-                CryptoCount delivers income statements to block reward holders, offering an accounting interface for users to visualize their block rewards.
+		CryptoCount supplies tax optimized financial data and accounting services for Tezos block reward delegators.				
                 <br />
 				<br />
+		The website visualizes block rewards and delivers income statements for users.	
+		<br />
                 Statements are generated when a user selects a quantity of rewards to realize.
+				<br/>
                 <br />
 				<br />
                 Downloadable statements can be delivered to authorities for tax reporting. 
                 <br />
 				<br />
                 This project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/about">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of the Portal To Blockchain Organization(PTBO) website. 
-                <br />
-				<br />
-                CryptoCount supplies tax optimized financial data and accounting services for Tezos block reward delegators.
                 <br />
 				<br />
                 The software was designed from literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of the PTBO website. 
