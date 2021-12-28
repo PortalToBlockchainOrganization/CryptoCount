3ac6bd18ea1d2cef72f21c86c36e2d21a4d54dc8
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2021-12-28 10:39:49 -0800

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
 				CryptoCount supports 40 countries and 1 blockchain. 
                 <br />
 				<br />
-                The project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/about">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of this site. 
+                The project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/client">Integrate CryptoCount</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of the PTBO website. 
                 <br />
 				<br />
                 The software was designed from economic literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of this site. 
