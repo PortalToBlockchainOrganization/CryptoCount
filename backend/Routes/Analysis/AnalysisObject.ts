33b530e87f5357d59dfa7973b7f5460c3ff3b3d5
Shaheen Alemi <sh.br.alemi@gmail.com>
2022-03-20 21:17:42 -0700

    creating sethistoryobject and analysis object skeletons

---
+++ backend/Routes/Analysis/AnalysisObject.ts
@@ -0,0 +1,29 @@
+class AnalysisObject {
+
+    unrealizedRewards: Map<Date, number>;
+    unrealizedBasisRewards: Map<Date, number>;
+    unrealizedBasisRewardsDepletion: Map<Date, number>;
+    unrealizedBasisRewardsMarketValueDilution: Map<Date, number>;
+    unrealizedRewardAggregate: number;
+    unrealizedBasisAggregate: number;
+    unrealizedDepletionAggregate: number;
+    unrealizedMarketValueDilutionAggregate: number;    
+    cryptoBasis: number;
+    basisPrice: number;
+    basisDepletion: number;
+    basisMarketValueDilution: number;
+    fiat: string;
+    address: string;
+    
+
+    constructor(){
+    
+    }
+
+    async analyze(rewards: Map<Date, number>, 
+        balances: Map<Date, number>, prices: Map<Date, number>, 
+        marketCap: Map<Date, number>){
+        // perform analysis and create analysis object
+    }
+
+}
