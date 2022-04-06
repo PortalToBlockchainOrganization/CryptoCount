a4e5e96622c2a75d5a60bf5bea110f2f2d1fbaf2
EC2 Default User <ec2-user@ip-172-31-15-31.us-west-2.compute.internal>
2022-04-06 20:13:39 +0000

    tezos contract transaction

--- backend/Routes/Analysis/tezTransOnContracts.js
+++ backend/Routes/Analysis/tezTransOnContracts.js
@@ -27,29 +27,29 @@ let axios = require("axios");
 
 
 
-async function getRewards(hash) {
-	//URL SET OBJECT CONSTRUCTION
+// async function getRewards(hash) {
+// 	//URL SET OBJECT CONSTRUCTION
  
-    let url = `https://api.tzkt.io/v1/operations/${hash}`
-    let data = await axios.get(url).then((d)=>{
-        console.log(d.data)
-        return d.data})
-    // }).then((d)=>{
-    // //     data.push(d)})
-    // let rewards = []
-    // for(i=0;i<data.length; i++){
-    //     reward = data[i].amount / 1000000
-    //     epoch = data[i].epoch
-    //     rewardObject = {
-    //         "reward": reward,
-    //         "epoch": epoch
-    //     }
-    //     rewards.push(rewardObject)
-    // }
+//     let url = `https://api.tzkt.io/v1/operations/${hash}`
+//     let data = await axios.get(url).then((d)=>{
+//         console.log(d.data)
+//         return d.data})
+//     // }).then((d)=>{
+//     // //     data.push(d)})
+//     // let rewards = []
+//     // for(i=0;i<data.length; i++){
+//     //     reward = data[i].amount / 1000000
+//     //     epoch = data[i].epoch
+//     //     rewardObject = {
+//     //         "reward": reward,
+//     //         "epoch": epoch
+//     //     }
+//     //     rewards.push(rewardObject)
+//     // }
 
-    // return rewards
+//     // return rewards
 	
-}
+// }
 
 // async function getRewards(address) {
 // 	//URL SET OBJECT CONSTRUCTION
@@ -77,29 +77,29 @@ async function getRewards(hash) {
 
 
 
-// async function getRewards(hash) {
-// 	//URL SET OBJECT CONSTRUCTION
+async function getRewards(address) {
+	//URL SET OBJECT CONSTRUCTION
  
-//     let url = `https://api.tzkt.io/v1/operations/transactions/${hash}`
-//     let data = await axios.get(url).then((d)=>{
-//         console.log(d.data)
-//         return d.data})
-//     // }).then((d)=>{
-//     // //     data.push(d)})
-//     // let rewards = []
-//     // for(i=0;i<data.length; i++){
-//     //     reward = data[i].amount / 1000000
-//     //     epoch = data[i].epoch
-//     //     rewardObject = {
-//     //         "reward": reward,
-//     //         "epoch": epoch
-//     //     }
-//     //     rewards.push(rewardObject)
-//     // }
+    let url = `https://api.tzkt.io/v1/operations/transactions?anyof.sender.target=${address}&limit=10000&amount.gt=1`
+    let data = await axios.get(url).then((d)=>{
+        console.log(d.data)
+        return d.data})
+    // }).then((d)=>{
+    // //     data.push(d)})
+    // let rewards = []
+    // for(i=0;i<data.length; i++){
+    //     reward = data[i].amount / 1000000
+    //     epoch = data[i].epoch
+    //     rewardObject = {
+    //         "reward": reward,
+    //         "epoch": epoch
+    //     }
+    //     rewards.push(rewardObject)
+    // }
 
-//     // return rewards
+    // return rewards
 	
-// }
+}
 
 
 
@@ -107,7 +107,8 @@ async function getRewards(hash) {
 
 
 //address = "KT1ErKVqEhG9jxXgUG2KGLW3bNM7zXHX8SDF"
-address ="opTn2gDWbmWtjdJbevW9DujNopP4Qppzffu6zwRK9CHQe1htZPW"
+//address ="opTn2gDWbmWtjdJbevW9DujNopP4Qppzffu6zwRK9CHQe1htZPW"
+address="KT1BJmsK1zhLy5PZNgC7YDfdjciAVTw1A5gq"
 getRewards(address).then((value) => {
     console.log(value);
     return value
