e1ecf71057334c7517247a0576b3f184b50c9e97
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2022-06-21 15:28:20 -0700

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -9,28 +9,28 @@ const About = () => {
 					About CryptoCount 	
 					<hr className={classes.HR} />
 				</h1>
-            <br />
+          <br />
 						
 				<br />
-		Welcome to CryptoCount V0.2. CryptoCount analyzes Tezos blockchain data to help Tezos delegators report their fair market value block reward tax income.				
+		CryptoCount 0.2 reads and analyzes the Tezos blockchain to help Tezonians report their fair tax income.				
                 <br />
 				<br />
-		CryptoCount is a tool for passively tracking your Tezos assets' paralellel performance in the fiat world.  
+		CryptoCount generates read-only tax accounting objects that track your Tezos assets. The object assesses your assets' paralellel performance in a selected fiat currency.  
 		<br />
+				<br />
+				Realize your assets to get FMV aggregate asset income statements. Save realizations to return to the set later with updated unrealized activities.
+				<br />
 				<br />
 				CryptoCount is 100% idempotent (read-only). CryptoCount will never: change the state of your Tezos address's related assets, ask for your private keys, or, any verifiable "KYC" information. 
 				<br />
 				<br />
-				V0.3 will feature interfaces accounting for: capital gains generated from realizing your Tezos-native basis, Tezos-token conversion capital gains, token-based asset sale capital gains, and, fair market value tracking of Defi activities (Liquidity, Lending, Revenue Sharing).
+				V 0.3 of CryptoCount (under development) supports tracking and realizing of native Tez capital gains, Tezos-based token capital gains, token-based asset capital gains, and FMV DeFi reward assessments (Liquidity, Lending, Revenue Sharing, etc.).
 				<br />
 				<br />
-                You can generate a statements by selecting a quantity of rewards to realize.
+				Downloadable statements can be delivered to authorities for income tax reporting. 
                 <br />
 				<br />
-				The downloadable statements can be delivered to authorities for income tax reporting. 
-                <br />
-				<br />
-				CryptoCount supports 40 countries and 1 the Tezos blockchain. 
+				CryptoCount supports 40 countries and Tezonians.  
                 <br />
 				<br />
                 CryptoCount supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/client">Integrate CryptoCount</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of the PTBO website. 
@@ -39,7 +39,7 @@ const About = () => {
                 This software was derived from economic literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of this site. 
                 <br />
 				<br />
-                Contact us <a href="https://www.portaltoblockchain.org">Here</a>. 
+                Feel free to contact us <a href="https://www.portaltoblockchain.org">Here</a> or join our <a href="https://discord.gg/7rYEu5c32E">Discord</a>. 
 			</div>
 		</div>
 	);
