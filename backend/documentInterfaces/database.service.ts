d781f751b0532f8505c3d1bbd4c5df387d5664f9
Shaheen Alemi <sh.br.alemi@gmail.com>
2022-06-20 22:36:28 -0700

    transcoded get rewards function to ts + db to ds

---
+++ backend/documentInterfaces/database.service.ts
@@ -0,0 +1,24 @@
+import * as mongoDB from "mongodb";
+import { ObjectId } from "mongodb";
+import CycleAndDate from "./CycleAndDate";
+
+const MONGOURI = `mongodb+srv://admin:lelloliar9876@postax.a1vpe.mongodb.net/AnalysisDep?retryWrites=true&w=majority`;
+const server = "127.0.0.1:27017";
+const database = "AnalysisDep";
+const cyclesCollectionString = "cycles";
+
+export const collections: { cycleAndDate?: mongoDB.Collection } = {}
+
+export async function connectToDatabase () { 
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGOURI);
+            
+    await client.connect();
+            
+    const db: mongoDB.Db = client.db(database);
+   
+    const cyclesCollection: mongoDB.Collection = db.collection(cyclesCollectionString);
+ 
+  collections.cycleAndDate = cyclesCollection;
+       
+         console.log(`Successfully connected to database: ${db.databaseName} and collection: ${cyclesCollection.collectionName}`);
+ }
