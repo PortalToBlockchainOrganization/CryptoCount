fb9db62c7962d55cb5ffc094c096a3a766263dd7
Shaheen Alemi <sh.br.alemi@gmail.com>
2022-06-20 22:36:28 -0700

    transcoded get rewards function to ts + db to ds

--- backend/Routes/Analysis/SetHistoryObjectImpl/TezosSetHistory.ts
+++ backend/Routes/Analysis/SetHistoryObjectImpl/TezosSetHistory.ts
@@ -6,21 +6,35 @@ import {AxiosResponse} from "axios";
 // data imports
 import TezosPricesAndMarketCap from "../../../model/blockchain.js";
 import TezosCycles from "../../../model/cycle.js";
-import { version } from "mongoose";
+import { version, Document, Model } from "mongoose";
+import CycleAndDate from "../../../documentInterfaces/CycleAndDate";
+import {collections, connectToDatabase} from "../../../documentInterfaces/database.service";
 import cycle from "../../../model/cycle.js";
 
 // define intermediary data holders as interfaces
 interface BakerCycle {
-    bakerAddress: String;
+    bakerAddress: string;
     cycleStart: number;
     cycleEnd: number;
     rewardsRequests: Array<string>;
 }
-
-interface RewardObject {
+interface PayoutCycleReward {
     quantity: number,
     cycle: number
 }
+
+interface RewardsByDay {
+    date: string,
+    rewardAmount: number,
+    cycle: number
+}
+
+interface FinalRewards {
+    date: Date,
+    rewardAmount: number
+}
+
+
 class TezosSet {
     
     walletAddress: string;
@@ -38,17 +52,24 @@ class TezosSet {
     }
 
     async getRewards(){
+        // method specific static strings
+        let transactionsUrl: string = `https://api.tzkt.io/v1/operations/transactions?anyof.sender.target=${this.walletAddress}`;
+        let delegatorRewardsUrl: string = `https://api.tzkt.io/v1/rewards/delegators/${this.walletAddress}?cycle.ge=0&limit=10000`;
+
+
+        await connectToDatabase();
+
         // get history of delegators bakers 
-        let url: string = `https://api.tzkt.io/v1/rewards/delegators/${this.walletAddress}?cycle.ge=0&limit=10000`;
-        let response:AxiosResponse = await axios.get(url);
+        let delegatorRewardsResponse: AxiosResponse = await axios.get(delegatorRewardsUrl);
 
         let filteredResponse:Array<{cycle: number, balance: number, baker: {alias: string, address: string}}> =
-            response.data.map(({cycle, balance, baker}) => ({cycle, balance, baker}));
+        delegatorRewardsResponse.data.map(({cycle, balance, baker}) => ({cycle, balance, baker}));
 
 
         // map bakers to their start and end cycles
 
         let bakerCylcles: Array<BakerCycle> = [];
+        let bakers: Set<string> = new Set<string>();
         let curBakerIndex: number = bakerCylcles.length;
         let curBaker: BakerCycle = undefined;
         
@@ -63,17 +84,20 @@ class TezosSet {
             }
             // push curBaker, start new baker
             else {
-                this.setRewardsUrls(curBaker)
-                bakerCylcles.push(curBaker)
+                this.setRewardsUrls(curBaker);
+                bakerCylcles.push(curBaker);
+                bakers.add(cycleData.baker.address);
                 curBaker = {bakerAddress: cycleData.baker.address, cycleStart: cycleData.cycle, cycleEnd: cycleData.cycle, rewardsRequests: []};
             } 
         
         }
         this.setRewardsUrls(curBaker)
         bakerCylcles.push(curBaker)
+        bakers.add(curBaker.bakerAddress)
 
         // put together all baker reward requests and call api to get payoutArrays
 
+        // requests are chunked in groups of 16 to prevent rate limiting issues
         let completeRewardsRequests: Array<string> = bakerCylcles.map(bakerCylcle => {return bakerCylcle.rewardsRequests}).flat()
         let j, temporary, chunk: number = 16;
         let responses: Array<AxiosResponse> = [];
@@ -81,42 +105,78 @@ class TezosSet {
         for (let i: number = 0,j = completeRewardsRequests.length; i < j; i += chunk) {
             temporary = completeRewardsRequests.slice(i, i + chunk);
             let response: Array<AxiosResponse> = await axios.all(temporary.map(url=> axios.get(url)));
-            // do whatever
             responses.push(...response)
         }
 
-        // filter payoutArrays to only get payout sent to delegator wallet
-        let rewardObjects: Array<RewardObject> = responses.map(response => {
-            let payouts: Array<number> = response.data.payouts;
-            if (payouts === undefined){
+
+        let rewards: Record<number, number> = {};
+        responses.forEach(response => {
+            if (response?.data?.payouts === undefined){
                 console.log("NO PAYOUT DATA ", response.data)
             }
             else{
-                return payouts.filter(payout => {
-                    this.walletAddress === payout["address"];
-                }).map(payout => {
-                    let amount: number = payout["amount"]
-                    if(amount < 0.0001 && amount > 0{
-                        amount = amount * 10000;
+                response.data.payouts.forEach(payout => {
+                    if(payout.address === this.walletAddress){
+                        let amount: number = payout["amount"]
+                        if(amount < 0.0001 && amount > 0) {
+                            amount = amount * 10000;
+                        }
+                        if(rewards[response.data.cycle]===undefined){
+                            rewards[response.data.cycle] = amount; 
+                        }
+                        else{
+                            rewards[response.data.cycle] += amount;
+                        }
                     }
-                    let rewardObject: RewardObject = {
-                        quantity: amount,
-                        cycle: response.data.cycle
-                    };
-                    return rewardObject
-            }))
+                })
+            }
+        });
+
+        
+        // get mapping of cycles to dates
+        const cyclesAndDates = (await collections.cycleAndDate.find({}).toArray()) as CycleAndDate[];
+        let rewardsByDay: Array<RewardsByDay> = cyclesAndDates.map(cycleAndDateDoc => {
+            return {date: cycleAndDateDoc.dateString, rewardAmount: rewards[cycleAndDateDoc.cycleNumber], cycle: cycleAndDateDoc.cycleNumber};
+        });
+
+        // get wallet transactions
+        let transactionsResponse: AxiosResponse = await axios.get(transactionsUrl);
+        console.log(transactionsResponse);
+        let transactions:Array<{target: {address: string}, sender: {address: string, alias: string}, amount: number, timestamp: string}> =
+        transactionsResponse.data.map(({target, sender, amount, timestamp}) => ({target, sender, amount, timestamp}));
+
+        let adjustedRewards: Array<FinalRewards> = transactions.map(transaction => {
+            let transactionDate: Date = new Date(transaction.timestamp);
+            let adjustedAmount: number = transaction.amount / 1000000;
+            let reward: FinalRewards = {date: transactionDate, rewardAmount: adjustedAmount};
+            if (transaction?.target?.address === this.walletAddress){
+                reward = {date: transactionDate, rewardAmount: adjustedAmount}
+            }
+            else if (transaction?.sender?.address === this.walletAddress){
+                reward.rewardAmount = adjustedAmount * -1;
+            }
+            if (!(bakers.has(transaction?.sender?.address) || bakers.has(transaction?.target?.address))) {
+                //check for baker transactions
+                return reward
             }
 
-        })
+
+        });
+
+        console.log(rewardsByDay, adjustedRewards);
+
+        // return [ rewardsByDay, adjustedRewards ];
 
     }
+        
+
+    
 
     // utility methods:
     setRewardsUrls(bakerData: BakerCycle){ 
         for (let i = bakerData.cycleStart; i <= bakerData.cycleEnd; i++) {
             bakerData.rewardsRequests.push(`https://api.baking-bad.org/v2/rewards/${bakerData.bakerAddress}?cycle=${i}`);
         }
-        
     }
         
 }
