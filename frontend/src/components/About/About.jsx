123b11578466344954f24f3a071bab9aa7e69384
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2022-07-21 12:37:08 -0700

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -12,24 +12,22 @@ const About = () => {
           <br />
 						
 				<br />
-		CryptoCount 0.2 reads and analyzes the Tezos blockchain to help Tezonians report their fair tax income.				
+		CryptoCount 0.2 reads and analyzes the Tezos blockchain to help report fair tax income from all DeFi activity.				
                 <br />
 				<br />
-		CryptoCount generates read-only tax accounting objects that track your Tezos assets. The object assesses your assets' paralellel performance in a selected fiat currency.  
+		CryptoCount generates read-only tax accounting objects that track your Tezos assets. The object assesses your assets' paralellel fair market value (FMV) performance in a selected fiat currency.  
 		<br />
 				<br />
-				Realize your assets to get FMV aggregate asset income statements. Save realizations to return to the set later with updated unrealized activities.
+				Realize your assets to get FMV aggregated income statements. Save realizations and return to the set later with updated unrealized activities.
 				<br />
 				<br />
 				CryptoCount is 100% idempotent (read-only). CryptoCount will never: change the state of your Tezos address's related assets, ask for your private keys, or, any verifiable "KYC" information. 
 				<br />
 				<br />
-				V 0.3 of CryptoCount (under development) supports tracking and realizing of native Tez capital gains, Tezos-based token capital gains, token-based asset capital gains, and FMV DeFi reward assessments (Liquidity, Lending, Revenue Sharing, etc.).
+				CryptoCount 0.3 (under development) supports tracking and realizing of all transaction based combinations of native Tez capital gains, Tezos-based token capital gains, token-based asset capital gains, and FMV DeFi reward assessments (Liquidity, Lending, Revenue Sharing, etc.).
 				<br />
 				<br />
-				Downloadable statements can be delivered to authorities for income tax reporting. 
-                <br />
-				<br />
+             
 				CryptoCount supports 40 countries and Tezonians.  
                 <br />
 				<br />
