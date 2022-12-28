b3a77cacd024df37ed1ddc9e90c74e667d9eabb9
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2022-12-28 14:48:11 -0800

    Update Landing.jsx

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -457,7 +457,7 @@ const Landing = (props) => {
 								<HelpOutlineRoundedIcon
 									className="helpIcon" />
 								</div>
-				<CopyToClipboard text={"63924e57daf4e1c27a7735c5"} onCopy={() => setIsCopied({isCopied: true})}>
+				<CopyToClipboard text={"634f4438724543fb97858be2"} onCopy={() => setIsCopied({isCopied: true})}>
 							<button className="words3"><span>{isCopied ? 'Copied' : 'Copy Set ID'}</span>
 							</button>
 						</CopyToClipboard>
