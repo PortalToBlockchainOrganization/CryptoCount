f0dbd8b5c0698ed85d73f7bce6fe24f94a40be6c
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2023-01-11 16:48:15 -0800

    Update index.js

--- backend/build/index.js
+++ backend/build/index.js
@@ -104,6 +104,10 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST, OPTIONS");
     next();
 });
+
+app.use(cors_1({
+    origin: '*'
+}));
 //app.all('/*', function (req, res, next) {
  //   res.header("Access-Control-Allow-Origin", "*");
   //  res.header("Access-Control-Allow-Headers", "X-Requested-With");
