579bc6bf9d37aa0ff8dc8dee26900ec2b92b6609
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2023-01-28 12:36:30 -0800

    Update NavbarComponent.js

--- frontend/src/components/Navbar/NavbarComponent.js
+++ frontend/src/components/Navbar/NavbarComponent.js
@@ -233,7 +233,7 @@ const NavbarComponent = ({ signedIn, signOut, user, canAccessAnalysis }) => {
 				className={classes.NameWrapper}
 				style={{ backgroundColor: "transparent" }}
 			>
-				<div className={classes.Beta}>Version<br></br> 0.2.3.1</div>
+				<div className={classes.Beta}>Version<br></br> 0.2.3.2</div>
 				{true ? (
 					<Navbar.Text className={classes.Name}>
 						{signedIn() ? (
